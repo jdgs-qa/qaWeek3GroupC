50266cefaaebccb3c4d7579272710585d24c22ec
bernardabraham <bernardabraham@academytrainee.com>
2019-06-28 13:56:15 +0100

    "last commit"

--- NewPets.js
+++ NewPets.js
@@ -1,60 +1,33 @@
-//"id": 1,
-    // "name": "Leo",
-    //     "birthDate": "2010/09/07",
-    //         "type": {
-    //     "id": 1,
-    //         "name
-// for an id print off pet name, birthDate, type, 
 
-// function displayPet(method, url, userData) {
-    
-//     makeRequest('GET', 'http://localhost:9966/petclinic/api/pets')
-//         .then((value) => {
-//             console.log("success");
-//             //const data = JSON.parse(req.responseText);
-//             for (key in value) {
-//                 if (data === userData.owner.Id) {
-//                      document.getElementById("name").value = key;
-
-//                      console.log("name");
-//                      console.log(key)
-//                      break;
-//                 }
-//             }
-//             console.log(value);
-//         }).catch((error) => {
-//             console.warn(error);
-//         });
-//     console.log(value); 
-//     document.getElementById(value).innerText;
-
-// }
-
-
-function displayPet(method, url, userData) {
-    console.log("Start");
-const name = "Franklin";
-
+//function displayPet() {
+    const name = "Franklin";
+    const user = sessionStorage.getItem("userData");
+    const userData = JSON.parse(user);
     makeRequest('GET', 'http://localhost:9966/petclinic/api/pets')
-        .then((value) => {
-            console.log("success");
-            //const data = JSON.parse(req.responseText);
-            for (key in value) {
-                if (name === userData.owner.Id) {
-                    document.getElementById("name").value = key;
-                    console.log("name");
-                    console.log(key)
-                    break;
+    .then((value) => {
+        const parent = document.getElementById("name")
+        console.log(value);
+        console.log(userData);
+
+            for (key of value) {
+                console.log(key);
+                console.log(key["owner"]["id"]);
+                
+                if (key["owner"]["id"] == userData["id"]) {
+                    
+                    // document.getElementById("catname").innerText = key["name"];
+                    const wrapperEl = document.createElement("p");
+                    wrapperEl.innerText = key["name"];
+                    parent.append(wrapperEl);
+                    console.log(key["owner"]["id"]);
+                    console.log(userData["id"]);
                 }
+
             }
-            console.log(value);
-            console.log("error")
+          
         }).catch((error) => {
             console.warn(error);
         });
-    console.log(value);
-    document.getElementById(value).innerText;
+    
 
-    console.log("End");
-
-}
+// }
