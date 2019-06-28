2180d0d8833b6416e1f0bee23ab504953edfc3ea
Jonathan <jonathan@Zeta7Omega2.local>
2019-06-28 13:54:51 +0100

    working!!!

--- getVisits.js
+++ getVisits.js
@@ -1,32 +1,41 @@
-window.onload = getVisits(1);
+window.onload = getVisits(7);
 
 function getVisits(petId) {
-    getPetVisits()
-    const petVisits = sessionStorage.getItem("petVisits");
-    for (let i of petVisits) {
-        let visitNo = petVisits[i];
-        if (visitNo.pet.id == petId) {
-            let y = i;
+    getPetVisits();
+    const petVisit = JSON.parse(sessionStorage.getItem("data"));
+    console.log("here it is:", petVisit);
+    console.log("does this exist?", petVisit[1]);
+    console.log("how about this?", petVisit[1].pet);
+    console.log("and this?", petVisit[1].pet.id);
+    for (let i in petVisit) {
+        const visitNo = petVisit[i];
+        if (visitNo.pet.id === petId) {
+            sessionStorage.setItem("vId", i);
         }
     }
+
+    const vNo = sessionStorage.getItem("vId");
+
     const date = document.getElementById("visitDate");
-    date.innerText = "Date of Visit: " + petVisits[y].date;
+    date.innerText = "Date of Visit: " + petVisit[vNo].date;
 
     const reason = document.getElementById("visitReason");
-    reason.innerText = "Reason for Visit: " + petVisits[y].description;
+    reason.innerText = "Reason for Visit: " + petVisit[vNo].description;
 
-    const pet = document.getElementById("visitReason");
-    pet.innerText = "Pet Name:  " + petVisits[y].pet.name;
+    const pet = document.getElementById("name");
+    pet.innerText = "Pet Name:  " + petVisit[vNo].pet.name;
+    return false;
 }
+
 
 function getPetVisits() {
     makeRequest('GET', 'http://localhost:9966/petclinic/api/visits/')
         .then((value) => {
             console.log("Visit details retrieved successfully!!", value);
-            const petVisits = value;
-            sessionStorage.setItem('petVisits', petVisits);
+            const petVisits = JSON.stringify(value);
+            sessionStorage.setItem("data", petVisits);
         }).catch((error) => {
             console.warn("It definitely didnt work... :(", error);
         });
-    return;
+    return false;
 }
